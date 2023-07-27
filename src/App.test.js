7860a98cae6f4fded4472a7024b2b40ca289fe77
Dasom Park <dev.somda@gmail.com>
2023-07-27 16:56:27 +0900

    ✅ test: add feature test code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("0이 잘 보인다!", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText("0")).toBeInTheDocument();
+});
+
+describe("더하기와 빼기", () => {
+  it("더하기를 누르면 1만큼 증가한다!", () => {
+    render(<App />);
+
+    const incrementButton = screen.getByRole("button", { name: "더하기" });
+    fireEvent.click(incrementButton);
+
+    expect(screen.getByText("1")).toBeInTheDocument();
+  });
+  it("빼기를 누르면 1만큼 감소한다!", () => {
+    render(<App />);
+
+    const decrementButton  = screen.getByRole("button", { name: "빼기" });
+    fireEvent.click(decrementButton );
+
+    expect(screen.getByText("-1")).toBeInTheDocument();
+  });
 });
